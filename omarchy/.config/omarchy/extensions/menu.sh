bed9f72bc925773895be20fe78b726622f15617c
Yordan Yordanov <yoyordanov94@gmail.com>
2026-04-29 11:22:45 +0300

    wip

--- omarchy/.config/omarchy/extensions/menu.sh
+++ omarchy/.config/omarchy/extensions/menu.sh
@@ -1,14 +1,18 @@
 # Override screenrecord menu to add "No audio" option
+# Uses local patched script (VFR instead of CFR) to avoid stutter on 59.95 Hz monitors
+# See: omarchy commit a4303e17 which added -fm cfr for kdenlive import convenience
+SCREENRECORD="$HOME/.config/omarchy/bin/omarchy-cmd-screenrecord"
+
 show_screenrecord_menu() {
-  omarchy-cmd-screenrecord --stop-recording && exit 0
+  "$SCREENRECORD" --stop-recording && exit 0
 
   case $(menu "Screenrecord" "  No audio\n  With desktop audio\n  With desktop + microphone audio\n  With desktop + microphone audio + webcam") in
-  *"No audio") omarchy-cmd-screenrecord ;;
-  *"With desktop audio") omarchy-cmd-screenrecord --with-desktop-audio ;;
-  *"With desktop + microphone audio") omarchy-cmd-screenrecord --with-desktop-audio --with-microphone-audio ;;
+  *"No audio") "$SCREENRECORD" ;;
+  *"With desktop audio") "$SCREENRECORD" --with-desktop-audio ;;
+  *"With desktop + microphone audio") "$SCREENRECORD" --with-desktop-audio --with-microphone-audio ;;
   *"With desktop + microphone audio + webcam")
     local device=$(show_webcam_select_menu) || { back_to show_capture_menu; return; }
-    omarchy-cmd-screenrecord --with-desktop-audio --with-microphone-audio --with-webcam --webcam-device="$device"
+    "$SCREENRECORD" --with-desktop-audio --with-microphone-audio --with-webcam --webcam-device="$device"
     ;;
   *) back_to show_capture_menu ;;
   esac
